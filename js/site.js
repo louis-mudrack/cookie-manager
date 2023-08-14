af2af670e574e2f417aed3c21a3459302e878169
Louis Mudrack <louis.mudrack@web.de>
2023-08-14 22:51:27 +0200

    redesigned the whole cookie manager

--- js/site.js
+++ js/site.js
@@ -1,16 +1,16 @@
 new PrivacyControl({
     cookies: {
-        googlemaps: {
+        youtube: {
             name: "YouTube",
             provider: "Google Ireland Limited",
             lang: "Bei der Nutzung dieses Dienstes werden Daten an Google über­mittelt, außer­dem ist es wahr­scheinlich, dass Google Daten (z. B. Cookies) auf Ihrem Gerät speichert.",
             privacy: "https://policies.google.com/privacy?hl=de&gl=de",
         },
-        youtube: {
+        googlemaps: {
             name: "Google Maps",
             provider: "Google Ireland Limited",
             lang: "Bei der Nutzung dieses Dienstes werden Daten an Google über­mittelt, außer­dem ist es wahr­scheinlich, dass Google Daten (z. B. Cookies) auf Ihrem Gerät speichert.",
             privacy: "https://policies.google.com/privacy?hl=de&gl=de",
-        },
+        }
     },
 });
